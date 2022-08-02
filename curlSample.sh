c9f0dc318151ca8d6303d8ab5513aa2bf4fc6fdc
yuya-0928 <yuniyuni0928@gmail.com>
2022-08-02 22:14:13 +0900

    initial commit

---
+++ curlSample.sh
@@ -0,0 +1,25 @@
+curl --http1.0 http://localhost:18888/greeting
+curl --http1.0 --get --data-urlencode "search word" http://localhost:18888
+
+# 9p
+curl -v --http1.0 http://localhost:18888/greeting
+
+# 13p
+curl -v --http1.0 -A "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)" http://localhost:18888
+
+# 38p
+curl --http1.0 -F title="The Art of Community" -F author="Jono Bacon" -F attachment-file=@test.txt http://localhost:18888
+
+# 39p
+curl --http1.0 -F attachment-file=@test.txt http://localhost:18888
+curl --http1.0 -F "attachment-file=@test.txt;type=text/html" http://localhost:18888
+curl --http1.0 -F "attachment-file=@test.txt;filename=sample.txt" http://localhost:18888
+
+# 44p
+curl --http1.0 --compressed http://localhost:18888
+
+# 46p
+curl --http1.0 -c cookie.txt -b cookie.txt -b "name=value" http://example.com/helloworld
+
+# 52p
+curl 
